460c70d693a890d66db0b53c934dc8c39de26ca8
choive <choive.co@gmail.com>
2026-04-02 14:24:47 +0000

    Create lead.js

---
+++ Choive/netlify/functions/lead.js
@@ -0,0 +1,25 @@
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Method Not Allowed'
+    };
+  }
+
+  try {
+    const data = JSON.parse(event.body);
+
+    console.log('New lead:', data);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true })
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Something went wrong' })
+    };
+  }
+};
