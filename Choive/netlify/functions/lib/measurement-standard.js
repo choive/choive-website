1faa531ee320586e11d8bbfbdbf80a855bab3b8c
choive <choive.co@gmail.com>
2026-08-18 10:37:47 +0200

    Update measurement-standard.js

--- Choive/netlify/functions/lib/measurement-standard.js
+++ Choive/netlify/functions/lib/measurement-standard.js
@@ -1,1 +1,19 @@
 
+'use strict';
+
+const STANDARD_NAME = 'CHOIVE AI Selection Standard';
+const STANDARD_VERSION = '3.0';
+const RUBRIC_VERSION = 'evidence-rubric-v3';
+const METHODOLOGY_URL = 'https://choive.com/methodology';
+
+function measurementProvenance(measuredAt) {
+  return {
+    name: STANDARD_NAME,
+    version: STANDARD_VERSION,
+    rubricVersion: RUBRIC_VERSION,
+    methodologyUrl: METHODOLOGY_URL,
+    measuredAt: measuredAt || new Date().toISOString()
+  };
+}
+
+module.exports = { STANDARD_NAME, STANDARD_VERSION, RUBRIC_VERSION, METHODOLOGY_URL, measurementProvenance };
